6413a365447cde02dbadef23a390b419e01db2ed
VO NGUYEN PHUONG DUY <duydc22v7n504@vlvh.ctu.edu.vn>
2026-03-24 09:27:18 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
+app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
